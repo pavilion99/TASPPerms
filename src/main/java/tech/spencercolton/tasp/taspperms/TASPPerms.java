d811c790ce3a18318c600efeba3a8298a21d6f98
Spencer Colton <me@spencercolton.tech>
2016-01-06 22:31:33 -0500

    Fix Code

--- src/main/java/tech/spencercolton/tasp/taspperms/TASPPerms.java
+++ src/main/java/tech/spencercolton/tasp/taspperms/TASPPerms.java
@@ -2,29 +2,17 @@ package tech.spencercolton.tasp.taspperms;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import tech.spencercolton.tasp.TASP;
 
 public class TASPPerms extends JavaPlugin {
 
-    public static TASP t;
-
     @Override
     public void onEnable() {
-        Plugin p = getServer().getPluginManager().getPlugin("TASP");
+        Plugin p = this.getServer().getPluginManager().getPlugin("TASP");
 
         if(p == null) {
-            getLogger().severe("TASP plugin not loaded.");
-            getServer().getPluginManager().disablePlugin(this);
+            this.getLogger().severe("TASP plugin not loaded.");
+            this.getServer().getPluginManager().disablePlugin(this);
         }
-
-        if(p instanceof TASP) {
-            t = (TASP)p;
-        } else {
-            getLogger().severe("A plugin called \"TASP\" seems to be loaded, but it is not the TASP I want.");
-            getServer().getPluginManager().disablePlugin(this);
-        }
-
-        tech.spencercolton.tasp.taspperms.Util.Config.loadConfig(this.getConfig());
     }
 
     @Override
